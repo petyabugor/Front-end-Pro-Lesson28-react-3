68c235032787e0f9bb7bbac332ed2b543253927a
Bugor_Petr <102902699+petyabugor@users.noreply.github.com>
2022-11-22 19:44:06 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Header, Main, Footer } from "./components/index";
+import "./App.css";
+function App() {
+  return (
+    <div className="wrapper">
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
